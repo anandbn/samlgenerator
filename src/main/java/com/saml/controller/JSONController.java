a37d591d6d43ad675ce4c05bec2ecb64c1b3f2f4
Anand BN <anarasimhan@salesforce.com>
2012-11-20 18:32:42 -0800

    Initial Commit

---
+++ src/main/java/com/saml/controller/JSONController.java
@@ -0,0 +1,49 @@
+package com.saml.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
+import org.opensaml.xml.io.MarshallingException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.saml.utils.SAML2AssertionGenerator;
+
+
+
+@Controller
+public class JSONController {
+
+	@RequestMapping(value="/saml" ,method = RequestMethod.GET)
+	public @ResponseBody Map<String,String> getAssertion(@RequestParam("username") String username) {
+		Map<String,String> retMap = new HashMap<String,String>();
+		try {
+			String samlResponse = SAML2AssertionGenerator.getSamlAssertion(username);
+			retMap.put("assertion",samlResponse);
+			retMap.put("status","success");
+			retMap.put("statusMsg","Generated SAML Assertion successfully !!!");
+		} catch (MarshallingException e) {
+			retMap.put("status","error");
+			retMap.put("statuMsg",e.getMessage());
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			retMap.put("status","error");
+			retMap.put("statuMsg",e.getMessage());
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			retMap.put("status","error");
+			retMap.put("statuMsg",e.getMessage());
+			e.printStackTrace();
+		}
+		retMap.put("username", username);
+		return retMap;
+	}
+	
+}
---
+++ src/main/java/com/saml/utils/SAML2AssertionGenerator.java
@@ -0,0 +1,53 @@
+package com.saml.utils;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
+import org.apache.commons.codec.binary.Base64;
+import org.opensaml.xml.io.MarshallingException;
+
+import axiom.saml.idp.IdentityProvider;
+import axiom.saml.idp.IdpConfiguration;
+import axiom.saml.idp.SamlUserIdLocation;
+import axiom.saml.idp.SamlVersion;
+import axiom.saml.idp.UserType;
+import axiom.saml.idp.XmlObjectSerializer;
+public class SAML2AssertionGenerator {
+	private static URL keyStoreFile=null;
+	
+	static{
+		keyStoreFile = SAML2AssertionGenerator.class.getClassLoader().getResource("AxiomIdpExample.keystore");
+		System.out.println("Loaded keystore file from :"+keyStoreFile);
+	}
+
+	public static String getSamlAssertion(String username) throws MarshallingException, TransformerFactoryConfigurationError, TransformerException{
+		// TODO Auto-generated method stub
+        IdentityProvider idp = new IdentityProvider(getIdpConfig(username));
+        String samlAssertion = XmlObjectSerializer.xmlObjectToString(idp.generateSamlResponse());
+        String base64Str = new String(Base64.encodeBase64(samlAssertion.getBytes()));
+        System.out.println("==============Assertion BASE64-START ===============");
+        System.out.println(base64Str);
+        System.out.println("==============Assertion BASE64-END   ===============");
+        return base64Str;
+	}
+ 	public static IdpConfiguration getIdpConfig(String username) {
+    	IdpConfiguration idpConfig = new IdpConfiguration();
+    	idpConfig.setKeystoreFile(keyStoreFile);
+        idpConfig.setSamlUserIdLocation(SamlUserIdLocation.SUBJECT);
+        idpConfig.setKeystoreAlias("axiom");
+        idpConfig.setRecipient("https://login.salesforce.com/services/oauth2/token");
+        idpConfig.setSamlVersion(SamlVersion._2_0);
+        idpConfig.setUserType(UserType.STANDARD);
+        idpConfig.setKeystorePassword("123456".toCharArray());
+        idpConfig.setKeystoreAliasPassword("123456".toCharArray());
+        idpConfig.setIssuer("http://www.chatterio.com");
+        idpConfig.setUserId(username);
+        return idpConfig;
+    }
+    
+
+}
